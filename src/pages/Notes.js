ae0a5c0e4b17840388640e048349190d94a84681
shawon122334 <shawon.mu11@gmail.com>
2021-05-12 00:56:50 +0600

    button added

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
-
+import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container'
 export default function Notes() {
   return (
-    <div>
+    <Container>
       <Typography variant="h6" component="h2" color="textSecondary" gutterBottom>
         Create A New Note 
       </Typography>
-    </div>
+      <Button onClick={()=>console.log('you clicked the button')} type="submit" color="primary" variant="contained">Submit</Button>
+    </Container>
   )
 }
