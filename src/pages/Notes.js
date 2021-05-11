65cb864e0f6f32055920b75a45a0b9edab72f778
shawon122334 <shawon.mu11@gmail.com>
2021-05-12 01:10:54 +0600

    buttons and icons added

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -2,13 +2,26 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
+import SendIcon from '@material-ui/icons/Send';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+
 export default function Notes() {
   return (
     <Container>
-      <Typography variant="h6" component="h2" color="textSecondary" gutterBottom>
+      <Typography 
+      variant="h6" 
+      component="h2" 
+      color="textSecondary" 
+      gutterBottom>
         Create A New Note 
       </Typography>
-      <Button onClick={()=>console.log('you clicked the button')} type="submit" color="primary" variant="contained">Submit</Button>
+      <Button onClick={()=>console.log('you clicked the button')} 
+      type="submit"
+      color="primary"
+      variant="contained"
+      startIcon={<SendIcon/>}
+      endIcon={<ArrowForwardIosIcon/>}
+      >Submit</Button>
     </Container>
   )
 }
