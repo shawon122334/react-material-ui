183930eac4117432777a895fecea647e18099f06
shawon122334 <shawon.mu11@gmail.com>
2021-05-12 00:33:15 +0600

    typography added|

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Typography from '@material-ui/core/Typography'
 
 export default function Notes() {
   return (
     <div>
-      Notes page
-      <h1>this is notes page </h1>
+      <Typography variant="h6" component="h2" color="textSecondary" gutterBottom>
+        Create A New Note 
+      </Typography>
     </div>
   )
 }
