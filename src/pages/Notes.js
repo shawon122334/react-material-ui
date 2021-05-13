88b742f69bef5e956c097bed9ff51ed799c923f0
shawon122334 <shawon.mu11@gmail.com>
2021-05-13 16:19:43 +0600

    font quicksand added

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -4,24 +4,33 @@ import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
 import SendIcon from '@material-ui/icons/Send';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import { makeStyles} from '@material-ui/core';
+import { purple } from '@material-ui/core/colors';
+
+
+const useStyles = makeStyles({
+
+})
+
 
 export default function Notes() {
+  const classes = useStyles();
   return (
-    <Container>
-      <Typography 
-      variant="h6" 
-      component="h2" 
-      color="textSecondary" 
-      gutterBottom>
-        Create A New Note 
+      <Container>
+        <Typography
+          variant="h6"
+          component="h2"
+          color="secondary"
+          gutterBottom>
+          Create A New Note
       </Typography>
-      <Button onClick={()=>console.log('you clicked the button')} 
-      type="submit"
-      color="primary"
-      variant="contained"
-      startIcon={<SendIcon/>}
-      endIcon={<ArrowForwardIosIcon/>}
-      >Submit</Button>
-    </Container>
+        <Button onClick={() => console.log('you clicked the button')}
+          type="submit"
+          color="primary"
+          variant="contained"
+          startIcon={<SendIcon />}
+          endIcon={<ArrowForwardIosIcon />}
+        >Submit</Button>
+      </Container>
   )
 }
