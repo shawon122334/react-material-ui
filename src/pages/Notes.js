fa7e5a403dff13f28703069d8ae82766925c227b
shawon122334 <shawon.mu11@gmail.com>
2021-05-14 20:45:53 +0600

    form validation

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,36 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
 import SendIcon from '@material-ui/icons/Send';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
-import { makeStyles} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
 import { purple } from '@material-ui/core/colors';
-
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles({
-
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: 'block'
+  }
 })
 
 
 export default function Notes() {
   const classes = useStyles();
+  const [title, setTitle] = useState('');
+  const [details, setDetails] = useState('');
+  const [titleError, setTitleError] = useState('');
+  const [detailsError, setDetailsError] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setTitleError(false);
+    setDetailsError(false);
+
+    if (title == '') {
+      setTitleError(true);
+    }
+    if(details == ''){
+      setDetailsError(true);
+    }
+
+    if (title && details) {
+      console.log(title, details)
+    }
+  }
   return (
-      <Container>
-        <Typography
-          variant="h6"
-          component="h2"
-          color="secondary"
-          gutterBottom>
-          Create A New Note
+    <Container>
+      <Typography
+        variant="h6"
+        component="h2"
+        color="secondary"
+        gutterBottom>
+        Create A New Note
       </Typography>
-        <Button onClick={() => console.log('you clicked the button')}
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          className={classes.field}
+          label="New Note"
+          variant="outlined"
+          color="primary"
+          fullWidth
+          required
+          error={titleError}
+        />
+        <TextField
+          onChange={(e) => setDetails(e.target.value)}
+          className={classes.field}
+          label="Details"
+          variant="outlined"
+          color="primary"
+          multiline
+          rows={4}
+          fullWidth
+          required
+          error={detailsError}
+        />
+        <Button
           type="submit"
           color="primary"
           variant="contained"
           startIcon={<SendIcon />}
           endIcon={<ArrowForwardIosIcon />}
         >Submit</Button>
-      </Container>
+      </form>
+
+    </Container>
   )
 }
