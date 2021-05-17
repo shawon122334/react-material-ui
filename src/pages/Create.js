b261ca3da856e13d84bfed3fc83e6f897deb123e
shawon122334 <shawon.mu11@gmail.com>
2021-05-18 00:14:04 +0600

    json server added

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -2,11 +2,15 @@ import React, { useState } from 'react'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
-import SendIcon from '@material-ui/icons/Send';
-import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
-import { FormControl, FormControlLabel, makeStyles, RadioGroup, Radio, FormLabel } from '@material-ui/core';
-import { purple } from '@material-ui/core/colors';
-import TextField from '@material-ui/core/TextField';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
+import { makeStyles } from '@material-ui/core'
+import TextField from '@material-ui/core/TextField'
+import Radio from '@material-ui/core/Radio'
+import RadioGroup from '@material-ui/core/RadioGroup'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import FormControl from '@material-ui/core/FormControl'
+import FormLabel from '@material-ui/core/FormLabel'
+import { useHistory } from 'react-router-dom'
 
 const useStyles = makeStyles({
   field: {
@@ -16,83 +20,91 @@ const useStyles = makeStyles({
   }
 })
 
-
 export default function Create() {
-  const classes = useStyles();
-  const [title, setTitle] = useState('');
-  const [details, setDetails] = useState('');
-  const [titleError, setTitleError] = useState('');
-  const [detailsError, setDetailsError] = useState('');
-  const [category, setCategory] = useState('money');
-
+  const classes = useStyles()
+  const history = useHistory()
+  const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+  const [titleError, setTitleError] = useState(false)
+  const [detailsError, setDetailsError] = useState(false)
+  const [category, setCategory] = useState('money')
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-
-    setTitleError(false);
-    setDetailsError(false);
+    e.preventDefault()
+    setTitleError(false)
+    setDetailsError(false)
 
     if (title == '') {
-      setTitleError(true);
+      setTitleError(true)
     }
     if (details == '') {
-      setDetailsError(true);
+      setDetailsError(true)
     }
-
     if (title && details) {
-      console.log(title, details, category)
-    }
+      fetch('http://localhost:8000/notes', {
+        method: 'POST',
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify({ title, details, category })
+      }).then(() => history.push('/'))
+    } 
   }
+
   return (
-    <Container>
+    <Container size="sm">
       <Typography
-        variant="h6"
+        variant="h6" 
+        color="textSecondary"
         component="h2"
-        color="secondary"
-        gutterBottom>
-        Create A New Note
+        gutterBottom
+      >
+        Create a New Note
       </Typography>
+      
       <form noValidate autoComplete="off" onSubmit={handleSubmit}>
-        <TextField
+        <TextField className={classes.field}
           onChange={(e) => setTitle(e.target.value)}
-          className={classes.field}
-          label="New Note"
-          variant="outlined"
-          color="primary"
+          label="Note Title" 
+          variant="outlined" 
+          color="secondary" 
           fullWidth
           required
           error={titleError}
         />
-        <TextField
+        <TextField className={classes.field}
           onChange={(e) => setDetails(e.target.value)}
-          className={classes.field}
           label="Details"
           variant="outlined"
-          color="primary"
+          color="secondary"
           multiline
           rows={4}
           fullWidth
           required
           error={detailsError}
         />
+
+        {/* <Radio value="hello" />
+        <Radio value="goodbye" /> */}
+
         <FormControl className={classes.field}>
           <FormLabel>Note Category</FormLabel>
           <RadioGroup value={category} onChange={(e) => setCategory(e.target.value)}>
-            <FormControlLabel value="money" control={< Radio />} label="Money" />
-            <FormControlLabel value="todos" control={< Radio />} label="ToDos" />
-            <FormControlLabel value="reminder" control={< Radio />} label="Reminder" />
-            <FormControlLabel value="work" control={< Radio />} label="Work" />
+            <FormControlLabel value="money" control={<Radio />} label="Money" />
+            <FormControlLabel value="todos" control={<Radio />} label="Todos" />
+            <FormControlLabel value="reminders" control={<Radio />} label="Reminders" />
+            <FormControlLabel value="work" control={<Radio />} label="Work" />
           </RadioGroup>
         </FormControl>
+
         <Button
-          type="submit"
-          color="primary"
+          type="submit" 
+          color="secondary" 
           variant="contained"
-          startIcon={<SendIcon />}
-          endIcon={<ArrowForwardIosIcon />}
-        >Submit</Button>
+          endIcon={<KeyboardArrowRightIcon />}>
+          Submit
+        </Button>
       </form>
 
+      
     </Container>
   )
 }
--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,18 +1,19 @@
-import React,{useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Notes() {
-  const [notes,setNotes] = useState([])
+  const [notes, setNotes] = useState([]);
+
   useEffect(() => {
     fetch('http://localhost:8000/notes')
-    .then(response => response.json())
-    .then(data=>setNotes(data))
-  }, []);
+      .then(res => res.json())
+      .then(data => setNotes(data))
+  }, [])
+
   return (
     <div>
-      {notes.map(note=> (
-        <p key={note.id}>{note.title}</p>
+      {notes.map(note => (
+        <p key={note.id}>{ note.title }</p>
       ))}
-      <p>hello</p>
     </div>
   )
 }
