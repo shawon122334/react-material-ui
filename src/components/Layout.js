88206d442e8d12a26fa7ab76e441d2703bf8c57a
shawon122334 <shawon.mu11@gmail.com>
2021-05-23 02:50:11 +0600

    drawer list done

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,65 +2,71 @@ import { Drawer, List, ListItem, ListItemIcon, ListItemText, makeStyles, Typogra
 import { AddCircleOutlineOutlined, SubjectOutlined } from '@material-ui/icons';
 import React from 'react'
 import { useHistory, useLocation } from 'react-router';
-const drawerWidth= 240;
-const useStyle= makeStyles({
-    page:{
-        backgroundColor:'#f9f9f9',
-        width:'100%',
-    },
-    drawer:{
-        width: drawerWidth
-    },
-    paperDrawer:{
-        width: drawerWidth
-    },
-    root:{
-        display: 'flex',
-    },
-    active:{
-        background:'#f4f4f4'
+const drawerWidth = 240;
+const useStyle = makeStyles((theme) => {
+    return {
+        page: {
+            backgroundColor: '#f9f9f9',
+            width: '100%',
+            padding: theme.spacing(3)
+        },
+        drawer: {
+            width: drawerWidth
+        },
+        paperDrawer: {
+            width: drawerWidth
+        },
+        root: {
+            display: 'flex',
+        },
+        active: {
+            background: '#f4f4f4'
+        },
+        title:{
+            padding: theme.spacing(2)
+        }
     }
 })
 export default function Layout({ children }) {
-    const classes=useStyle()
-    const menuItems =[
+    const classes = useStyle()
+    const menuItems = [
         {
             text: "My Notes",
-            icon: <SubjectOutlined color="primary"/>,
+            icon: <SubjectOutlined color="primary" />,
             path: '/',
         },
         {
             text: "Create Notes",
-            icon: <AddCircleOutlineOutlined color="primary"/>,
+            icon: <AddCircleOutlineOutlined color="primary" />,
             path: '/create',
         }
     ]
-    const history=useHistory()
-    const location=useLocation()
+    const history = useHistory()
+    const location = useLocation()
     return (
         <div className={classes.root}>
-            <Drawer 
-            className={classes.drawer}
-            variant="permanent"
-            anchor="left"
-            classes={{paper: classes.paperDrawer}}
+            <Drawer
+                className={classes.drawer}
+                variant="permanent"
+                anchor="left"
+                classes={{ paper: classes.paperDrawer }}
             >
-            {/* classes is a built in method here to override css  */}
+                {/* classes is a built in method here to override css  */}
                 <div>
-                    <Typography variant="h5">
+                    <Typography variant="h5" className={classes.title}>
                         Ninja Notes
                     </Typography>
                 </div>
                 <List>
-                    {menuItems.map(item=>(
+                    {menuItems.map(item => (
                         <ListItem
-                        button 
-                        key={item.text}
-                        onClick={()=>history.push(item.path)}
-                        className={location.pathname == item.path ? classes.active : null}
+                            button
+                            key={item.text}
+                            onClick={() => history.push(item.path)}
+                            className={location.pathname == item.path ? classes.active : null}
                         >
                             <ListItemIcon>{item.icon}</ListItemIcon>
-                            <ListItemText primary={item.text}/>
+                            <ListItemText primary={item.text} />
                         </ListItem>
                     ))}
                 </List>
--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,16 +1,27 @@
-import { Card, CardContent, CardHeader, IconButton, Typography } from '@material-ui/core'
+import { Card, CardContent, CardHeader, IconButton, makeStyles, Typography } from '@material-ui/core'
 import React from 'react'
-import {DeleteOutlined} from '@material-ui/icons';
+import { DeleteOutlined } from '@material-ui/icons';
 
-export default function NoteCard({ note , handleDelete}) {
+const useStyle = makeStyles({
+    test: {
+        border: (note) => {
+            if (note.category == 'work') {
+                return '1px solid red'
+            }
+        }
+    }
+    //made a conditional style 
+})
+export default function NoteCard({ note, handleDelete }) {
+    const classes = useStyle(note)
     return (
         <div>
-            <Card elevation={2}>
+            <Card elevation={2} className={classes.test}>
                 <CardHeader
                     action={
-                        <IconButton onClick={()=>handleDelete(note.id)} >
-                        <DeleteOutlined/>
-                    </IconButton>
+                        <IconButton onClick={() => handleDelete(note.id)} >
+                            <DeleteOutlined />
+                        </IconButton>
                     }
                     title={note.title}
                     subheader={note.category}
@@ -20,7 +31,7 @@ export default function NoteCard({ note , handleDelete}) {
                         {note.details}
                     </Typography>
                 </CardContent>
-                
+
             </Card>
         </div>
     )
