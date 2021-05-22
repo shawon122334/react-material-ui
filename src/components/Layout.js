c5a0974e4b6127819ae8f738b94505d6074f2c52
shawon122334 <shawon.mu11@gmail.com>
2021-05-23 01:28:21 +0600

    drawer added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,38 @@
-import { makeStyles } from '@material-ui/core'
+import { Drawer, makeStyles, Typography } from '@material-ui/core'
 import React from 'react'
+const drawerWidth= 240;
 const useStyle= makeStyles({
     page:{
         backgroundColor:'#f9f9f9',
         width:'100%',
+    },
+    drawer:{
+        width: drawerWidth
+    },
+    paperDrawer:{
+        width: drawerWidth
+    },
+    root:{
+        display: 'flex',
     }
 })
 export default function Layout({ children }) {
     const classes=useStyle()
     return (
-        <div>
+        <div className={classes.root}>
+            <Drawer 
+            className={classes.drawer}
+            variant="permanent"
+            anchor="left"
+            classes={{paper: classes.paperDrawer}}
+            >
+            {/* classes is a built in method here to override css  */}
+                <div>
+                    <Typography variant="h5">
+                        Ninja Notes
+                    </Typography>
+                </div>
+            </Drawer>
             <div className={classes.page}>
                 {children}
             </div>
